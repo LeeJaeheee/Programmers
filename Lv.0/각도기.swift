6b7292993173c33c328742126ab74f823810fb70
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-04 08:09:44 +0900

    [Programmers_120829] 각도기

---
+++ Lv.0/각도기.swift
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120829
+
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    return angle < 90 ? 1 : angle == 90 ? 2 : angle < 180 ? 3 : 4
+}
