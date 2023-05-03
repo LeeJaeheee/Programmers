0013e636815d6c13fbcb6f9c025e5c73d1e22c53
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-04 08:03:22 +0900

    [Programmers_120806] 두 수의 나눗셈

---
+++ Lv.0/두 수의 나눗셈.swift
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return Int(Double(num1) / Double(num2) * 1000)
+}
