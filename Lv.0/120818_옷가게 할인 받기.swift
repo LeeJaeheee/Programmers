16d4bbccc5a195f6b715c7ec5122bf7e4592f229
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-05 07:18:39 +0900

    [Programmers_120818] 옷가게 할인 받기

---
+++ Lv.0/120818_옷가게 할인 받기.swift
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120818
+
+import Foundation
+
+func solution(_ price:Int) -> Int {
+    switch price {
+    case 500000... : return Int(Double(price) * 0.8)
+    case 300000... : return Int(Double(price) * 0.9)
+    case 100000... : return Int(Double(price) * 0.95)
+    default : return price
+    }
+}
