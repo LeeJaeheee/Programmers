06f3be3881c49d027d9379f94031512bd3146aff
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-04 09:31:22 +0900

    [Programmers_120833] 배열 자르기

---
+++ Lv.0/배열 자르기.swift
@@ -0,0 +1,17 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120833
+
+import Foundation
+
+func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+    return (num1...num2).map { numbers[$0] }
+}
+
+
+// 다른 풀이
+import Foundation
+
+func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+    return Array(numbers[num1...num2])
+}
+
+// (Arrayslice) https://developer.apple.com/documentation/swift/arrayslice
