b98628631f85c10743cbc2341e080a4765750783
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-04 09:06:02 +0900

    [Programmers_120809] 배열 두배 만들기

---
+++ Lv.0/배열 두배 만들기.swift
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    return numbers.map { $0 * 2 }
+}
+
+// (map)https://developer.apple.com/documentation/swift/array/map(_:)-87c4d
