95da932af9621d7d8dca9dd70bd51f573d9ec5e3
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-05 09:02:11 +0900

    [Programmers_120909] 제곱수 판별하기

---
+++ Lv.0/120909_제곱수 판별하기.swift
@@ -0,0 +1,10 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120909
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    let sqrt = Int(Double(n).squareRoot())
+    return sqrt * sqrt == n ? 1 : 2
+}
+
+// (squareRoot) https://developer.apple.com/documentation/swift/double/squareroot()
