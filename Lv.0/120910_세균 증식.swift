e004211de0481ffe4301d0aef502ef2a67a880b4
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-08 22:36:45 +0900

    [Programmers_120910] 세균 증식 ⭐️ / pow

---
+++ Lv.0/120910_세균 증식.swift
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120910
+
+import Foundation
+
+func solution(_ n:Int, _ t:Int) -> Int {
+    return n * Int(pow(2.0, Double(t)))
+}
+
+// (pow) https://developer.apple.com/documentation/foundation/1779833-pow
