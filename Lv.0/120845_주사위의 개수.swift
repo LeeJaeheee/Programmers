3402b9e311674d5b79522f0053ae5cb9b577ad8b
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-08 22:52:41 +0900

    [Programmers_120845] 주사위의 개수

---
+++ Lv.0/120845_주사위의 개수.swift
@@ -0,0 +1,15 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120845
+
+import Foundation
+
+func solution(_ box:[Int], _ n:Int) -> Int {
+    return (box[0] / n) * (box[1] / n) * (box[2] / n)
+}
+
+
+// 다른 풀이
+import Foundation
+
+func solution(_ box:[Int], _ n:Int) -> Int {
+    return box.map { $0 / n }.reduce(1, *)
+}
