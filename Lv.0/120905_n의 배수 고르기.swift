41d5c9bb18b5f3407046aa030a24acdb338ce3f5
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-05-08 23:26:10 +0900

    [Programmers_120905] n의 배수 고르기

---
+++ Lv.0/120905_n의 배수 고르기.swift
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120905
+
+import Foundation
+
+func solution(_ n:Int, _ numlist:[Int]) -> [Int] {
+    return numlist.filter { $0 % n == 0 }
+}
