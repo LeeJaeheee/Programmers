4fa61db94ba47ad4457dae7efc347b39e645f4fa
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-06-05 12:49:36 +0900

    [Programmers_120899] 가장 큰 수 찾기 ⭐️ / max(), index(of:)

--- Lv.0/120899_가장 큰 수 찾기.swift
+++ Lv.0/120899_가장 큰 수 찾기.swift
@@ -6,3 +6,6 @@ func solution(_ array:[Int]) -> [Int] {
     let max = array.max()!
     return [max, array.index(of: max)!]
 }
+
+// https://developer.apple.com/documentation/swift/array/max()
+// https://developer.apple.com/documentation/swift/int/words-swift.struct/index(of:)
