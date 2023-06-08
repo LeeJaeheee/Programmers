a0d6259f970a4bf8645b4e5873ed5b1eb4e8b169
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-06-09 08:38:22 +0900

    Update [Lv.3] 보석 쇼핑.swift

--- 2020 카카오 인턴십/[Lv.3] 보석 쇼핑.swift
+++ 2020 카카오 인턴십/[Lv.3] 보석 쇼핑.swift
@@ -31,3 +31,78 @@ func solution(_ gems:[String]) -> [Int] {
     return result
 }
 */
+/*
+import Foundation
+
+func solution(_ gems:[String]) -> [Int] {
+    let gemsSet = Set(gems)
+    let slen = gemsSet.count
+    let gemsCount = gems.count
+    
+    var result = [1, gemsCount]    
+    var start = 0
+    var end = start+slen-1
+    
+    var counts: [String: Int] = [:]
+    var nowSet: Set<String> = []
+    
+    for i in start...end {
+        nowSet.insert(gems[i])
+        counts[gems[i], default: 0] += 1
+    }
+    
+    while start <= gemsCount-slen && start+slen-1..<gemsCount ~= end {
+        if nowSet == gemsSet {  
+            while nowSet == gemsSet {
+                result = end-start < result[1]-result[0] ? [start+1, end+1] : result
+                if end-start+1 == slen {
+                    return result
+                }
+                if counts[gems[start]]! > 1 {
+                    counts[gems[start]]! -= 1
+                } else {
+                    nowSet.remove(gems[start])
+                    counts[gems[start]]! -= 1
+                }
+                start += 1
+            }
+        }
+        end += 1
+        if end < gemsCount {
+            nowSet.insert(gems[end])
+            counts[gems[end], default: 0] += 1
+        }
+    }
+
+    return result
+}
+*/
+/*
+import Foundation
+
+func solution(_ gems:[String]) -> [Int] {
+    let setCount = Set(gems).count
+    var result = [1, gems.count]
+    var gemDict: [String: Int] = [:]
+    var isTrue = false
+
+    for (i, gem) in gems.enumerated() {
+        gemDict[gem, default: 0] = i+1
+        if !isTrue {
+            isTrue = gemDict.count == setCount ? true : false
+        }
+        if isTrue {
+            let max = gemDict.values.max()!
+            let min = gemDict.values.min()!
+            if max - min == setCount - 1 {
+                return [min, max]
+            }
+            if result[1] - result[0] > max - min {
+                result = [min, max]
+            }
+        }
+    }
+    return result
+
+}
+*/
