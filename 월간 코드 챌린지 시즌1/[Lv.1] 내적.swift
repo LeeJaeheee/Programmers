b0705928115a184f062c40623edc922e64b812c3
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-06-19 21:24:43 +0900

    [Programmers_70128] 내적

---
+++ 월간 코드 챌린지 시즌1/[Lv.1] 내적.swift
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/70128
+
+import Foundation
+
+func solution(_ a:[Int], _ b:[Int]) -> Int {
+    return (0..<a.count).map{a[$0] * b[$0]}.reduce(0, +)
+}
